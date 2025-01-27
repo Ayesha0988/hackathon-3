1ba2335aa615e099ba6620aa34363b4e0dd25dda
Aisha saeed <aishasaeedr987@gmail.com>
2025-01-28 02:14:52 +0500

    commit

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,12 +1,14 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-const {createClient} = require('@sanity/client')
+import { createClient } from '@sanity/client';
 
 const client = createClient({
   projectId: '1h7orxsv',
   dataset: 'production',
   apiVersion: '2021-10-21', 
-  token: "skn1LX2J7s2y3daqCIYLjhEVzQTp6SjhIyxo6JRxY6zrJdJwajhwQt4HjTAk6hfRQtbfw3ZojwlrFC8GMV6ZIEWD2BNh1eOl2dIRroHe2eLhLydH2dT02ZH0kRzlFBT4vO8BRoU1xPdGv15USEfNoWplq92aAT3mAuUBWjGKLCvLYJ592Sfa", // or leave blank for unauthenticated usage
-  useCdn: false, 
-})
+  token: process.env.SANITY_TOKEN,  // Fetch from env variable
+  useCdn: false,
+});
+
+export { client };
